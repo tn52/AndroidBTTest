2e1eef0dcba9f48d57380c8f988523a0f499a744
Admin <admin@Admins-MBP.T-mobile.com>
2016-10-08 03:45:31 -0700

    bluetooth implemented

--- app/src/main/java/com/example/admin/bluetoothtest/MainActivity.java
+++ app/src/main/java/com/example/admin/bluetoothtest/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.admin.bluetoothtest;
 
+import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Set;
+import java.util.UUID;
+
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
@@ -20,12 +29,23 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
     private static final int REQUEST_ENABLE_BT = 1;
-    private Button onBtn, offBtn, listBtn, findBtn;
-    private TextView statusText;
+    private Button onBtn, offBtn, listBtn, findBtn, listenBtn;
+    private TextView statusText, dataText;
     private BluetoothAdapter myBluetoothAdapter;
     private Set<BluetoothDevice> pairedDevices;
     private ListView myListView;
     private ArrayAdapter<String> BTArrayAdapter;
+
+    BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice;
+    InputStream mmInputStream;
+    InputStreamReader mmInputStreamReader;
+    BufferedReader mmBufferedReader;
+    Thread workerThread;
+    byte[] readBuffer;
+    int readBufferPosition;
+    volatile boolean stopWorker;
+    boolean listening = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +65,7 @@ public class MainActivity extends Activity {
                     Toast.LENGTH_LONG).show();
         } else {
             statusText = (TextView) findViewById(R.id.text);
+            dataText = (TextView) findViewById(R.id.dataSentTextView);
             onBtn = (Button)findViewById(R.id.turnOn);
             onBtn.setOnClickListener(new OnClickListener() {
 
@@ -85,11 +106,30 @@ public class MainActivity extends Activity {
                 }
             });
 
+            listenBtn = (Button)findViewById(R.id.receiveDataButton);
+            listenBtn.setOnClickListener(new OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    // TODO Auto-generated method stub
+                    listen(v);
+                }
+            });
+
             myListView = (ListView)findViewById(R.id.listView1);
 
             // create the arrayAdapter that contains the BTDevices, and set it to the ListView
             BTArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
             myListView.setAdapter(BTArrayAdapter);
+        }
+    }
+
+    public void listen(View view){
+        listening = !listening;
+        String status = listening? "is listening" : "is not listening";
+        dataText.setText("Status: " + status);
+        if(listening) {
+            openBT();
         }
     }
 
@@ -169,5 +209,109 @@ public class MainActivity extends Activity {
         super.onDestroy();
         unregisterReceiver(bReceiver);
     }
+
+
+    /**
+     * THIS IS THE IMPORTANT CODE THAT NEEDS TO BE FIXED!
+     */
+
+    void openBT()
+    {
+        try {
+            for(BluetoothDevice device : pairedDevices) {
+                Log.d("listing paired devices", device.getName());
+                mmDevice = device;
+            }
+
+            try {
+                UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //Standard SerialPortService ID
+                Log.d("Name connected device:", mmDevice.getName());
+                mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+                mmSocket.connect();
+                mmInputStream = mmSocket.getInputStream();
+                mmInputStreamReader = new InputStreamReader(mmInputStream);
+                mmBufferedReader = new BufferedReader(mmInputStreamReader);
+            }catch(IOException e){
+                Log.e("", "Could not connect to device");
+            }
+
+            workerThread = new Thread(new Runnable() {
+                public void run() {
+
+                    while (!Thread.currentThread().isInterrupted() && !stopWorker) {
+//                        Log.d("test", "test");
+
+                        /*
+                            Right now the below commented code does not work, because there is no BT device
+                            sending data, so it cannot read from buffered reader. It might work with a BT device
+                            actively/constantly sending data.
+                            Useful link/resource: http://stackoverflow.com/questions/10996803/read-data-from-paired-bluetooth-devices-in-android
+                         */
+
+//                        try {
+//                            String s = mmBufferedReader.readLine();
+//                            Log.d("test", s);
+//                            dataText.setText(s);
+//                        }catch(Exception IOException) {
+//                        }
+
+                    }
+                }
+            });
+            workerThread.run();
+
+        }catch (Exception IOException){
+            Log.i("openBT", "IOException");
+        }
+    }
+
+//    void beginListenForData()
+//    {
+//        final byte delimiter = 10; //This is the ASCII code for a newline character
+//
+//        stopWorker = false;
+//        readBufferPosition = 0;
+//        readBuffer = new byte[1024];
+//        workerThread = new Thread(new Runnable()
+//        {
+//            public void run()
+//            {
+//                while(!Thread.currentThread().isInterrupted() && !stopWorker)
+//                {
+//                    try
+//                    {
+//                        int bytesAvailable = mmInputStream.available();
+//                        if(bytesAvailable > 0)
+//                        {
+//                            byte[] packetBytes = new byte[bytesAvailable];
+//                            mmInputStream.read(packetBytes);
+//                            for(int i=0;i<bytesAvailable;i++)
+//                            {
+//                                byte b = packetBytes[i];
+//                                if(b == delimiter)
+//                                {
+//                                    byte[] encodedBytes = new byte[readBufferPosition];
+//                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+//                                    final String data = new String(encodedBytes, "US-ASCII");
+//                                    readBufferPosition = 0;
+//
+//                                }
+//                                else
+//                                {
+//                                    readBuffer[readBufferPosition++] = b;
+//                                }
+//                            }
+//                        }
+//                    }
+//                    catch (IOException ex)
+//                    {
+//                        stopWorker = true;
+//                    }
+//                }
+//            }
+//        });
+//
+//        workerThread.start();
+//    }
 
 }
